8d18114f795728c9b41c3663b4609da5107f3ba2
Joanna Kowal <aska.szewczyk@gmail.com>
2018-05-09 13:13:54 +0200

    stworzenie div w js

--- ajax/js/main.js
+++ ajax/js/main.js
@@ -1,15 +1,19 @@
-$(function(){
+$(function () {
     'use strict';
-    
-    $("#btn").click(function() {
+
+    $("#btn").click(function () {
         var dane = "";
-        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data) {
-            dane = ("Dane programisty: imię: " + data.imie + ", nazwisko: " + data.nazwisko + ", zawód: " + data.zawod + ", firma: " + data.firma );
-            document.getElementById("output").innerHTML = dane;
+       
+        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
+              $('#btn').after($('<div/>', {
+                id: 'dane-programisty'
+            }));
+            dane = ("Dane programisty: imię: " + data.imie + ", nazwisko: " + data.nazwisko + ", zawód: " + data.zawod + ", firma: " + data.firma);
+            document.getElementById("dane-programisty").innerHTML = dane;
         });
-        
+
     });
-    
-    
-    
+
+
+
 });
