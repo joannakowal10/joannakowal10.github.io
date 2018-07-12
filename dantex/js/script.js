1d7de5a99e72da04e300f4d2d35ad063979332ba
joannaszewczykkowal <joanna.szewczyk.kowal@gmail.com>
2018-07-12 12:46:09 +0200

    stworzenie podstron

--- dantex/js/script.js
+++ dantex/js/script.js
@@ -1,6 +1,21 @@
 'use strict';
 $(document).ready(function () {
-    $("#btn-red").click(function () {
-        $("#content-inner").addClass("remove");
+    $("#btn-accept").click(function () {
+        $("#accept").show();
+        $("#content-inner").hide();
     });
+   $(".btn-return").click(function (){
+      $("#accept").hide();
+        $("#content-inner").show();
+   });
+    $("#btn-resignation").click(function () {
+        $("#form-section").show();
+        $("#content-inner").hide();
 });
+$("#send").click(function () {
+        $("#resignation").show();
+        $("#form-section").hide();
+});
+   
+    })
+     
