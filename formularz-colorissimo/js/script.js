516668b2fe869b53646b64caa7f07e49f92dbeac
joannaszewczykkowal <joanna.szewczyk.kowal@gmail.com>
2018-08-02 15:00:53 +0200

    submit

--- formularz-colorissimo/js/script.js
+++ formularz-colorissimo/js/script.js
@@ -1,10 +1,27 @@
-$(function (){
-    $("#link-remind").click(function() {                $("#remind-panel a").click();
+$(function () {
+    $("#link-remind").click(function () {
+        $("#remind-panel a").click();
     });
-    
-     $("#link-return").click(function() {               
-    $("#log-panel a").click();
+
+    $("#link-return").click(function () {
+        $("#log-panel a").click();
+    });
+
+    function showText() {
+        $('#btn-log span').removeClass("hide-text");
+        $('#btn-log').prop("disabled", false);
+        $('.sk-double-bounce').hide();
+    };
+
+    $("#log-form").submit(function (event) {
+
+        $('#btn-log span').addClass("hide-text");
+        $('.sk-double-bounce').show();
+        $('#btn-log').prop('disabled', true);
+        setTimeout(showText, 4000);
+
+
+        return false
+
     });
 });
-
-//onclick='$("#remind-panel a").click()'
