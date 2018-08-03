a6c3a99cf8f24d8f1f86cd714b68b0b2a483ef30
joannaszewczykkowal <joanna.szewczyk.kowal@gmail.com>
2018-08-03 11:45:42 +0200

    zmiana kodu

--- formularz-colorissimo/js/script.js
+++ formularz-colorissimo/js/script.js
@@ -7,18 +7,32 @@ $(function () {
         $("#log-panel a").click();
     });
 
-    function showText() {
-        $('#btn-log span').removeClass("hide-text");
-        $('#btn-log').prop("disabled", false);
-        $('.spinner').hide();
+//    function showText() {
+//        $('#btn-log span').removeClass("hide-text");
+//        $('#btn-log').prop("disabled", false);
+//        $('.spinner').hide();
+//    };
+//
+//    $("#log-form").submit(function (event) {
+//
+//        $('#btn-log span').addClass("hide-text");
+//        $('.spinner').show();
+//        $('#btn-log').prop('disabled', true);
+//        setTimeout(showText, 4000);
+    
+    
+    function unblockButton() {
+//        $('.btn-default span').removeClass("hide-text");
+        $('.btn-default').prop("disabled", false);
+//        $('.spinner').hide();
     };
 
-    $("#log-form").submit(function (event) {
+    $("form").submit(function (event) {
 
-        $('#btn-log span').addClass("hide-text");
-        $('.spinner').show();
-        $('#btn-log').prop('disabled', true);
-        setTimeout(showText, 4000);
+//        $('.btn-default span').addClass("hide-text");
+//        $('.spinner').show();
+        $('.btn-default').prop('disabled', true);
+        setTimeout(unblockButton, 4000);
 
 
         return false
