9f8b04657a5c69f0382d535cc4b53b3f5a692a9f
joannaszewczykkowal <joanna.szewczyk.kowal@gmail.com>
2018-08-03 14:15:09 +0200

    ODC

--- formularz-colorissimo/js/script.js
+++ formularz-colorissimo/js/script.js
@@ -7,37 +7,33 @@ $(function () {
         $("#log-panel a").click();
     });
 
-    
+
     function unblockButton() {
-//        $('.btn-default span').removeClass("hide-text");
+
         $('.btn-default').prop("disabled", false);
-//        $('.spinner').hide();
     };
 
-    $("form").submit(function (event) {
+    $("#log-form").submit(function (event) {
 
-//        $('.btn-default span').addClass("hide-text");
-//        $('.spinner').show();
-//        $('.btn-default').prop('disabled', true);
-//        setTimeout(unblockButton, 4000);
-          var $buttonClick = $(this).find('button');
-       
+
+        var $buttonClick = $(this).find('button');
+
         $buttonClick.attr("disabled", true);
-         setTimeout(unblockButton, 4000);
-//        jQuery.post('https://colorissimo.com/default/login/process', {
-//referrer: '/', 
-//   login: 'kamil@goodcode.pl', //zastapic danymi z inputa
-//   pass: 'h77912', // zastapic danymi z inputa
-//   remember_me: 0
-//}, function(response) {
-//if(data.status == 1 && data.redirect != undefined) {
-//       console.log('zalogowano');
-//   } else {
-//       console.log('blad.');
-//       console.log('odblokuj przycisk.');
-//       console.log('wyswietl komunikat bledu.');
-//   }
-//})
+        setTimeout(unblockButton, 4000);
+        jQuery.post('https://colorissimo.com/default/login/process', {
+            referrer: '/',
+            login: $('#user').val(),
+            pass: $('log-password').val(),
+            remember_me: 0
+        }, function (response) {
+
+            if (response.status == 1 && response.redirect != undefined) {
+                window.location = response.redirect;
+            } else {
+                unblockButton();
+$('.message').innerHTML("Error");
+            }
+        });
 
         return false
 
