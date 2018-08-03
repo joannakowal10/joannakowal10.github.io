e623b28874681aa118c3f24a8d3633001e201788
joannaszewczykkowal <joanna.szewczyk.kowal@gmail.com>
2018-08-03 12:18:53 +0200

    zmiany

--- formularz-colorissimo/js/script.js
+++ formularz-colorissimo/js/script.js
@@ -9,18 +9,15 @@ $(function () {
 
     
     function unblockButton() {
-//        $('.btn-default span').removeClass("hide-text");
+//   
         $('.btn-default').prop("disabled", false);
-//        $('.spinner').hide();
+//     
     };
 
     $("form").submit(function (event) {
-
-//        $('.btn-default span').addClass("hide-text");
-//        $('.spinner').show();
-        $('.btn-default').prop('disabled', true);
-        setTimeout(unblockButton, 4000);
-
+          var $buttonClick = $(this).find('button');
+       
+        $buttonClick.attr("disabled", true);
 
         return false
 
