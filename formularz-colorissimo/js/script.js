de19303265fb51d39209c2d0aa8aa100005d7fed
joannaszewczykkowal <joanna.szewczyk.kowal@gmail.com>
2018-08-07 11:46:25 +0200

    dv

--- formularz-colorissimo/js/script.js
+++ formularz-colorissimo/js/script.js
@@ -11,7 +11,7 @@ $(function () {
         $('.btn-default').prop("disabled", false);
     };
 
-    $("#log-form").submit(function (event) {
+    $("#logForm").submit(function (event) {
 
         var $buttonClick = $(this).find('button').first();
 
@@ -33,32 +33,71 @@ $(function () {
             }
         });
 
-//        return false
+        return false
 
     });
 
 
-    $().ready(function () {
 
-        $("#reg-form").validate({
-            rules: {
-                companyName: {
-                    required: true,
-                    minlength: 3
-                },
-                street: {
-                    required: true},
-                email: {
-                    required: true,
-                    email: true,
-                },
-            },
-            messages: {
-                companyName: {
-                    require: "Proszę podać nazwę firmy",
-                },
-            }
 
-        });
+    $("#regForm").submit(function (event) {
+        console.log("shbsdhc");
+        return false;
+    });
+
+    $("#regForm").validate({
+        rules: {
+            companyName: "required",
+            street: "required",
+            city: "required",
+            zip: "required",
+            email: {required: true,
+                   email: true},
+            mobile: "required",
+            website: "required",
+             name: "required",
+             surname: "required",
+             yourEmail: "required",
+             regPassword: "required",
+            defaultCheck1: {required: "#defultCheck1:checked"}, 
+            defaultCheck2: "required", 
+            
+            
+            
+        },
+
+        messages: {
+            companyName: "Podaj nazwę firmy",
+            street: "Podaj nazwę ulicy",
+            city: "Podaj nazwę miasta",
+            zip: "Podaj kod pocztowy",
+            email: {required: "Wpisz adres e-mail",
+                   email: "Twój adres musi zawierać @"},
+            mobile: "Podaj numer telefonu",
+            website: "Podaj adres strony internetowej",
+            name: "Podaj swoje imię",
+             surname: "Podaj swoje nazwisko",
+             yourEmail: "Podaj swój adres e-mail",
+             regPassword: "Podaj hasło",
+            defaultCheck1: "Zaznacz to pole jeśli chcesz kontynować", 
+            defaultCheck2: "Zaznacz to pole jeśli chcesz kontynować",
+            
+
+        }
+
+
+
+        //
+        //        $("#regForm").validate({
+        //            rules: {
+        //                companyName: "required"
+        //            },
+        //            
+        //            messages: {
+        //                companyName: "Proszę podać nazwę firmy"
+        //            }
+        //        });
+
+
     });
 });
